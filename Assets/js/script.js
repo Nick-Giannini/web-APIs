36940fe6ce34a0f793122a516d5d9ba9e983cfba
Tazzok <131045020+Tazzok@users.noreply.github.com>
2023-05-22 18:25:37 -0500

    question list

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,57 +8,46 @@ var questList = [
 
     },
     {
-        question: " ",
-        posAns: [' ',' ',' ', ' '],
-        correctAns: ' ' ,
+        question: 'The "function" and "var" are known as:',
+        posAns: ['Keywords',' Data types','Declaration statements', 'Prototypes'],
+        correctAns: 'Declaration statements ' ,
 
     },   
     {
-        question: " ",
-        posAns: [' ',' ',' ', ' '],
-        correctAns: ' ' ,
+        question: "In JavaScript the x===y statement implies that:",
+        posAns: ['Both x and y are equal in value, type and reference address as well.','Both are x and y are equal in value only. ','Both are equal in the value and data type. ', 'Both are not same at all. '],
+        correctAns: 'Both are equal in the value and data type. ' ,
 
     },
     {
-        question: " ",
-        posAns: [' ',' ',' ', ' '],
-        correctAns: ' ' ,
+        question: "Which of the following attribute is used to provide a unique name to an element? ",
+        posAns: ['class ',' id','type ', 'None of the above '],
+        correctAns: 'id ' ,
 
     },
     {
-        question: " ",
-        posAns: [' ',' ',' ', ' '],
-        correctAns: ' ' ,
+        question: " How to create a checkbox in HTML? ",
+        posAns: ['<input type = "checkbox"> ','<input type = "button"> ','<checkbox> ', '<input type = "check">'],
+        correctAns: '<input type = "checkbox"> ' ,
 
     },
     {
-        question: " ",
-        posAns: [' ',' ',' ', ' '],
-        correctAns: ' ' ,
+        question: "How to select the elements with the class name 'example'? ",
+        posAns: ['example ','#example ','.example ', 'Class example '],
+        correctAns: '.example ' ,
 
     },
     {
-        question: " ",
-        posAns: [' ',' ',' ', ' '],
-        correctAns: ' ' ,
+        question: "Which of the following number object function returns the value of the number? ",
+        posAns: ['toString()','valueOf()','toLocaleString()', 'toPrecision()'],
+        correctAns: 'valueOf() ' ,
 
     },
     {
-        question: " ",
-        posAns: [' ',' ',' ', ' '],
-        correctAns: ' ' ,
+        question: "Which one of the following operator returns false if both values are equal? ",
+        posAns: ['! ','!== ','!= ', 'All of the above '],
+        correctAns: '!= ' ,
 
     },
-    {
-        question: " ",
-        posAns: [' ',' ',' ', ' '],
-        correctAns: ' ' ,
 
-    },
-    {
-        question: " ",
-        posAns: [' ',' ',' ', ' '],
-        correctAns: ' ' ,
-
-    },
 ]
