8fbf3eda6c05947c6d9588c1a130d623946d612f
Tazzok <131045020+Tazzok@users.noreply.github.com>
2023-05-22 18:03:50 -0500

    setting up array

---
+++ Assets/js/script.js
@@ -0,0 +1,64 @@
+//Creating an array of object questions
+
+var questList = [
+    {
+        question: " Javascript is an _______ language?",
+        posAns: ['Object-Oriented','Object-Based','Procedural', 'None of the above '],
+        correctAns: 'Object-Oriented',
+
+    },
+    {
+        question: " ",
+        posAns: [' ',' ',' ', ' '],
+        correctAns: ' ' ,
+
+    },   
+    {
+        question: " ",
+        posAns: [' ',' ',' ', ' '],
+        correctAns: ' ' ,
+
+    },
+    {
+        question: " ",
+        posAns: [' ',' ',' ', ' '],
+        correctAns: ' ' ,
+
+    },
+    {
+        question: " ",
+        posAns: [' ',' ',' ', ' '],
+        correctAns: ' ' ,
+
+    },
+    {
+        question: " ",
+        posAns: [' ',' ',' ', ' '],
+        correctAns: ' ' ,
+
+    },
+    {
+        question: " ",
+        posAns: [' ',' ',' ', ' '],
+        correctAns: ' ' ,
+
+    },
+    {
+        question: " ",
+        posAns: [' ',' ',' ', ' '],
+        correctAns: ' ' ,
+
+    },
+    {
+        question: " ",
+        posAns: [' ',' ',' ', ' '],
+        correctAns: ' ' ,
+
+    },
+    {
+        question: " ",
+        posAns: [' ',' ',' ', ' '],
+        correctAns: ' ' ,
+
+    },
+]
