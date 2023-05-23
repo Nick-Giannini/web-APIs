26d6a674729dca5ecf563b7d3d27705addaca374
Tazzok <131045020+Tazzok@users.noreply.github.com>
2023-05-22 21:37:59 -0500

    count down

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,50 +4,72 @@ var questList = [
     {
         question: " Javascript is an _______ language?",
         posAns: ['Object-Oriented','Object-Based','Procedural', 'None of the above '],
-        correctAns: 'Object-Oriented',
+        correctAns: 0,
 
     },
     {
         question: 'The "function" and "var" are known as:',
         posAns: ['Keywords',' Data types','Declaration statements', 'Prototypes'],
-        correctAns: 'Declaration statements ' ,
+        correctAns: 2,
 
     },   
     {
         question: "In JavaScript the x===y statement implies that:",
         posAns: ['Both x and y are equal in value, type and reference address as well.','Both are x and y are equal in value only. ','Both are equal in the value and data type. ', 'Both are not same at all. '],
-        correctAns: 'Both are equal in the value and data type. ' ,
+        correctAns: 2 ,
 
     },
     {
         question: "Which of the following attribute is used to provide a unique name to an element? ",
         posAns: ['class ',' id','type ', 'None of the above '],
-        correctAns: 'id ' ,
+        correctAns: 1,
 
     },
     {
         question: " How to create a checkbox in HTML? ",
         posAns: ['<input type = "checkbox"> ','<input type = "button"> ','<checkbox> ', '<input type = "check">'],
-        correctAns: '<input type = "checkbox"> ' ,
+        correctAns: 0,
 
     },
     {
         question: "How to select the elements with the class name 'example'? ",
         posAns: ['example ','#example ','.example ', 'Class example '],
-        correctAns: '.example ' ,
+        correctAns: 2,
 
     },
     {
         question: "Which of the following number object function returns the value of the number? ",
         posAns: ['toString()','valueOf()','toLocaleString()', 'toPrecision()'],
-        correctAns: 'valueOf() ' ,
+        correctAns: 1 ,
 
     },
     {
         question: "Which one of the following operator returns false if both values are equal? ",
         posAns: ['! ','!== ','!= ', 'All of the above '],
-        correctAns: '!= ' ,
+        correctAns: 2 ,
 
     },
+];
 
-]
+var timerDisplay = document.getElementById('timer');
+var secondsLeft = 75;
+var qcount = 0;
+
+
+function startTimer() {
+
+    var timerstart = setInterval(function() {
+      secondsLeft--;
+
+      if(secondsLeft > 1) {
+        timerDisplay.textContent = "Time: "+ secondsLeft;
+        //call question function
+      }
+      else if(secondsLeft === 1){
+        clearInterval(timerstart);
+      // Calls function to create and append the highscore page
+      // highscore();)
+      }
+  
+    }, 75000);
+  };
